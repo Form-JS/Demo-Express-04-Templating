a17e96f6e6cf06d7c1197959d8681c950e6c864c
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-08 11:45:11 +0100

    Add schema for pattern MVC ♥

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const chalk = require('chalk');
 const express = require('express');
+const chalk = require('chalk');
+const path = require('path');
+const homeRouter = require('./routes/home-router');
 
+// Variable de config
 const port = 8080;
+const rootDir = process.cwd();
+
+// Créer le serveur Web
 const app = express();
 
+// Configurer le moteur de vue
+// - Express va utiliser le moteur de vue automatiquement
+app.set('view engine', 'ejs');
+// - Configuration du répertoire dans lequel sont le vues
+app.set('views', path.resolve(rootDir, 'views'));
 
+// Ajout des routers
+app.use(homeRouter);
 
-
-
+// Demarrage du serveur
 app.listen(port, () => {
     console.log(chalk.magenta(`Server up on port ${port}`));
 });
