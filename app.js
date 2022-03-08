b5f534fff37dc4a6cb728564dc0c3d04d085f803
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-08 09:45:39 +0100

    Fix Chalk - Version 4

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const chalk = require('chalk');
 const express = require('express');
+
+const port = 8080;
+const app = express();
+
+
+
+
+
+app.listen(port, () => {
+    console.log(chalk.magenta(`Server up on port ${port}`));
+});
